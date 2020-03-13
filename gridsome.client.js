b694c7fd28eb3ed62c23208abb2bfbc7b01eb495
Brandon Pittman <brandon@brandonpittman.net>
2020-03-13 17:53:42 +0900

    Rollback

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,13 +1,5 @@
 import ogp from './lib/ogp'
 
-export default function (Vue) {
+export default function (Vue, _options, {router, head}) {
   Vue.prototype.$ogp = ogp
-
-  router.beforeEach((to, _from, next) => {
-    head.meta.push({
-      property: 'og:url',
-      content: process.env.GRIDSOME_BASE_PATH + to.path,
-    })
-    next()
-  })
 }
