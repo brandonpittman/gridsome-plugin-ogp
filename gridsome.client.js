ad9a790f2d1ceadeaaba7300f5849f1926295587
Brandon Pittman <brandon@brandonpittman.net>
2020-03-13 17:58:07 +0900

    Fix typo

--- gridsome.client.js
+++ gridsome.client.js
@@ -2,4 +2,14 @@ import ogp from './lib/ogp'
 
 export default function (Vue, _options, {router, head}) {
   Vue.prototype.$ogp = ogp
+
+  if (!process.env.GRIDSOME_BASE_PATH) return
+
+  router.beforeEach((to, _from, next) => {
+    head.meta.push({
+      property: 'og:url',
+      content: process.env.GRIDSOME_BASE_PATH + to.path,
+    })
+    next()
+  })
 }
