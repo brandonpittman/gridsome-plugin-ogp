b09ba4c223344dde6e32b665814fc23df1ad4172
Brandon Pittman <brandon@brandonpittman.net>
2020-03-13 15:49:24 +0900

    ✨ Auto set OGP URL using router.beforeEach hook
    Picked this idea up from DOCC

--- gridsome.client.js
+++ gridsome.client.js
@@ -2,4 +2,12 @@ import ogp from './lib/ogp'
 
 export default function (Vue) {
   Vue.prototype.$ogp = ogp
+
+  router.beforeEach(to, from, next) {
+    head.meta.push({
+      property: 'og:url',
+      content: process.env.GRIDSOME_BASE_PATH + to.path,
+    })
+    next()
+  }
 }
