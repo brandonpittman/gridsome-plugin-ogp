10bd811dbb5df68394c661c107f29f0683499bf2
Brandon Pittman <brandon@brandonpittman.net>
2020-03-13 22:04:44 +0900

    Fix og:url error

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,15 +1,5 @@
-import ogp from './lib/ogp'
+import ogp from './lib/ogp';
 
-export default function (Vue, _options, {router, head}) {
-  Vue.prototype.$ogp = ogp
-
-  if (!process.env.GRIDSOME_BASE_PATH) return
-
-  router.beforeEach((to, _from, next) => {
-    head.meta.push({
-      property: 'og:url',
-      content: process.env.GRIDSOME_BASE_PATH + to.path,
-    })
-    next()
-  })
+export default function (Vue) {
+  Vue.prototype.$ogp = ogp;
 }
